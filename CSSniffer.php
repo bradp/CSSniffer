ea2630ee2ea0c423222f6eff29cc01a64c92b457
Brad Parbs <brad@onyourmark.com>
2011-12-30 13:26:34 -0600

    First commit of WP Plugin!

---
+++ CSSniffer.php
@@ -0,0 +1,141 @@
+<?php
+
+/*
+Plugin Name: CSSniffer
+Plugin URI: http://wispress.com/
+Description: CSSniffer checks a users browser user agent, and serves up different CSS.
+Version: 0.1
+Author: OnYourMark, LLC
+Author URI: http://onyourmark.com/
+*/
+
+/**
+ * Copyright (c) 2012 OnYourMark, LLC. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+add_action('wp_head','cssniffer');
+add_action( 'admin_init', 'theme_options_init' );
+add_action( 'admin_menu', 'theme_options_add_page' );
+
+
+function cssniffer(){
+	//check to see what the user agent is
+
+	$firefox = strpos($_SERVER["HTTP_USER_AGENT"], 'Firefox') ? true : false;
+	$grossitsie = strpos($_SERVER["HTTP_USER_AGENT"], 'MSIE') ? true : false;
+	$safari = strpos($_SERVER["HTTP_USER_AGENT"], 'Safari') ? true : false;
+	$chrome = strpos($_SERVER["HTTP_USER_AGENT"], 'Chrome') ? true : false;
+	$opera = strpos($_SERVER["HTTP_USER_AGENT"], 'Opera') ? true : false;
+	$iphone = strpos($_SERVER["HTTP_USER_AGENT"], 'iPhone') ? true : false;
+	//this is static
+	echo '<!-- cssniffer -->';
+	echo '<style type="text/css">';
+
+	$options = get_option('cssniffer_theme_options');
+
+
+	if($firefox){
+	echo $options['firefox'];
+	}
+	if($chrome||$safari){
+	echo $options['webkit'];
+	}
+	if($opera){
+	echo $options['opera'];
+	}
+	if($grossitsie){
+	echo $options['grossitsie'];
+	}
+	if($iphone){
+	echo $options['iphone'];
+	}
+	echo'</style>';
+}
+
+
+function theme_options_do_page() {
+	if ( ! isset( $_REQUEST['settings-updated'] ) )
+		$_REQUEST['settings-updated'] = false;
+	?>
+	<div class="wrap">
+		<?php screen_icon(); echo "<h2>" . get_current_theme() . __( ' Theme Options', 'cssniffertheme' ) . "</h2>"; ?>
+
+		<?php if ( false !== $_REQUEST['settings-updated'] ) : ?>
+		<div class="updated fade"><p><strong><?php _e( 'Options saved', 'cssniffertheme' ); ?></strong></p></div>
+		<?php endif; ?>
+
+		<form method="post" action="options.php">
+			<?php settings_fields( 'cssniffer_options' ); ?>
+			<?php $options = get_option( 'cssniffer_theme_options' ); ?>
+
+			<table class="form-table">
+
+
+				<tr valign="top"><th scope="row"><?php _e( 'Firefox Specific CSS', 'cssniffertheme' ); ?></th>
+					<td>
+						<textarea id="cssniffer_theme_options[firefox]" class="large-text" cols="50" rows="10" name="cssniffer_theme_options[firefox]"><?php echo esc_textarea( $options['firefox'] ); ?></textarea>
+					</td>
+				</tr>
+				<tr valign="top"><th scope="row"><?php _e( 'Webkit Specific CSS', 'cssniffertheme' ); ?></th>
+					<td>
+						<textarea id="cssniffer_theme_options[webkit]" class="large-text" cols="50" rows="10" name="cssniffer_theme_options[webkit]"><?php echo esc_textarea( $options['webkit'] ); ?></textarea>
+					</td>
+				</tr>
+				<tr valign="top"><th scope="row"><?php _e( 'Internet Explorer Specific CSS', 'cssniffertheme' ); ?></th>
+					<td>
+						<textarea id="cssniffer_theme_options[grossitsie]" class="large-text" cols="50" rows="10" name="cssniffer_theme_options[grossitsie]"><?php echo esc_textarea( $options['grossitsie'] ); ?></textarea>
+					</td>
+				</tr>
+				<tr valign="top"><th scope="row"><?php _e( 'Opera Specific CSS', 'cssniffertheme' ); ?></th>
+					<td>
+						<textarea id="cssniffer_theme_options[opera]" class="large-text" cols="50" rows="10" name="cssniffer_theme_options[opera]"><?php echo esc_textarea( $options['opera'] ); ?></textarea>
+					</td>
+				</tr>	
+				<tr valign="top"><th scope="row"><?php _e( 'iPhone Specific CSS', 'cssniffertheme' ); ?></th>
+					<td>
+						<textarea id="cssniffer_theme_options[iphone]" class="large-text" cols="50" rows="10" name="cssniffer_theme_options[iphone]"><?php echo esc_textarea( $options['iphone'] ); ?></textarea>
+					</td>
+				</tr>
+
+			</table>
+			<p class="submit">
+				<input type="submit" class="button-primary" value="<?php _e( 'Save Options', 'cssniffertheme' ); ?>" />
+			</p>
+		</form>
+	</div>
+	<?php
+}
+
+
+function theme_options_validate( $input ) {
+	$input['firefox'] = wp_filter_post_kses( $input['firefox'] );
+	$input['webkit'] = wp_filter_post_kses( $input['webkit'] );
+	$input['opera'] = wp_filter_post_kses( $input['opera'] );
+	$input['grossitsie'] = wp_filter_post_kses( $input['grossitsie'] );
+	$input['iphone'] = wp_filter_post_kses( $input['iphone'] );
+
+	return $input;
+}
+
+
+
+function theme_options_add_page() {	add_theme_page( __( 'CSSniffer', 'cssniffertheme' ), __( 'CSSniffer', 'cssniffertheme' ), 'edit_theme_options', 'theme_options', 'theme_options_do_page' );}
+function theme_options_init(){register_setting( 'cssniffer_options', 'cssniffer_theme_options', 'theme_options_validate' );}
+?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== CSSniffer ===
+Contributors: bradparbs
+Donate link: http://onyourmark.com/
+Tags: css, browser, development
+Requires at least: 2.0.2
+Tested up to: 3.3
+Stable tag: 0.1
+
+Easily add styles for specific browsers and rendering engines, from the dashboard!
+
+== Description ==
+
+This plugin sniffs users agents, and provides a set of theme options for a user to put browser-specific CSS styles. It's very easy to use, very simple, and very powerful.
+
+== Installation ==
+
+1. Upload `CSSniffer.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Click of 'CSSniffer' in the Appearance tab.
+4. Hack some CSS like no tomorrow!
+
+== Frequently Asked Questions ==
+
+= What browsers can I target? =
+
+Right now, you can specifiy CSS rules for Internet Explorer, Firefox, Webkit, Opera, and iPhone.
+
+
+== Changelog ==
+
+= 0.1 =
+* Offcial release of CSSniffer
